e397a5cee3827bfce7b68b78ad7bad6f41a4e63a
Matheus <matheeuzinhoo282@gmail.com>
2026-04-05 14:57:24 -0300

    MemeTonervoso

---
+++ Program.cs
@@ -0,0 +1,9 @@
+﻿string fraseResposta, memeToNervoso = "Seu filha de uma";
+
+Console.Write("Fala oque voce acha de mim?");
+fraseResposta = Console.ReadLine()!;
+
+ memeToNervoso = 
+    $"{memeToNervoso.Remove(10)}... NÃO PERA. \n to Nervoso"!;
+
+Console.WriteLine(memeToNervoso);
